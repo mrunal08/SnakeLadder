e67b8977fcce7844db27dcb044e43cb757dc6bea
mrunal08 <mrunalg96@gmail.com>
2021-08-13 18:50:08 +0530

    UC1 First Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Snake And Ladder Program");
-       
-        
+
+      //UC1
+        final int START=0;
+        final int FINISH=100;
+        int playerPos;
+
+        System.out.println("Game Starts");
+        playerPos=START;
+        System.out.println("Player at position : "+playerPos);
     }
 }
