6a9c008fd7288fa9d7d5f2f5b80398f3c42a0fef
mrunal08 <mrunalg96@gmail.com>
2021-08-13 14:54:36 +0530

    First Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake And Ladder Program");
+       
+        
+    }
+}
