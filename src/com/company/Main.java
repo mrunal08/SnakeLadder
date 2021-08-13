60b24b53658d29a251f1f4d28760baf0b8c802c9
mrunal08 <mrunalg96@gmail.com>
2021-08-13 19:13:40 +0530

    UC2 Dice Random Number added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Snake And Ladder Program");
-       
-        
+
+        //UC2
+        int dice;
+        Random diceroll = new Random();
+        dice = diceroll.nextInt(6);
+        System.out.println("Dice Rolled : "+dice);
     }
 }
